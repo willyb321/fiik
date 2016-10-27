24b20e4c8230130b1a47502ac8650f9ba2727274
William Blythe <wbwilliam7@gmail.com>
2016-10-28 08:35:07 +1100

    initial 

---
+++ fiik/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  fiik
+//
+//  Created by william blythe on 28/10/2016.
+//  Copyright © 2016 william blythe. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
